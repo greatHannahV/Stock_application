7ac2fae9b189f2704bae10daca6766fd12a0a4dc
Hanna Veleva <i.kasl@mail.ru>
2025-05-16 22:50:52 +0300

    reinstalled vitest

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'tradingapplication',
   plugins: [react()],
   test: {
     environment: 'jsdom',
